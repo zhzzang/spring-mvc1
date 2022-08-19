14a2276e82fc779ace8e2ccd33082a83117288db
zhzzang <leejh0799@gmail.com>
2022-08-20 04:54:40 +0900

    상품 등록 컨트롤러와 뷰

--- item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -33,6 +33,11 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
     /**
      * 테스트용 데이터 추가
      */
